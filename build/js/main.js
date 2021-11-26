d6cbeae9a1141409e48bf6682e56926effd9645a
EMedved-bit <eymedvedeva.spb@gmail.com>
2021-11-26 09:54:36 +0300

    Реализация попапа обратной связи

---
+++ build/js/main.js
@@ -0,0 +1,88 @@
+const iMask = window.IMask;
+const openPopupButton = document.querySelector('.page-header__button');
+const popup = document.querySelector('.popup');
+const closePopupButton = popup.querySelector('.popup__close-button');
+const closeOverlay = popup.querySelector('.popup__overlay');
+
+const formPopup = document.querySelector('.popup .form');
+const userNamePopup = formPopup.querySelector('#name-popup');
+const userPhonePopup = formPopup.querySelector('#phone-popup');
+const userMessagePopup = formPopup.querySelector('#user-question-popup');
+
+const form = document.querySelector('.form form');
+const userName = form.querySelector('#name');
+const userPhone = form.querySelector('#phone');
+const userMessage = form.querySelector('#user-question');
+
+iMask(userPhonePopup, {
+  mask: '+{7}(000)000-00-00'
+});
+
+iMask(userPhone, {
+  mask: '+{7}(000)000-00-00'
+});
+
+let isStorageSupport = true;
+let storageName = '';
+let storagePhone = '';
+let storageMessage = '';
+
+try {
+  storageName = localStorage.getItem('userName');
+  storagePhone = localStorage.getItem('userPhone');
+  storageMessage = localStorage.getItem('userMessage');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (storageName) {
+  userNamePopup.value = storageName;
+  userName.value = storageName;
+}
+
+if (storagePhone) {
+  userPhonePopup.value = storagePhone;
+  userPhone.value = storagePhone;
+}
+
+if (storageMessage) {
+  userMessagePopup.value = storageMessage;
+  userMessage.value = storageMessage;
+}
+
+
+openPopupButton.addEventListener('click', function () {
+  popup.classList.add('opened');
+  userNamePopup.focus();
+});
+
+closePopupButton.addEventListener('click', function () {
+  popup.classList.remove('opened');
+});
+
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    event.preventDefault();
+    popup.classList.remove('opened');
+  }
+});
+
+closeOverlay.addEventListener('click', function () {
+  popup.classList.remove('opened');
+});
+
+formPopup.addEventListener('submit', function () {
+  if (isStorageSupport) {
+    localStorage.setItem('userName', userNamePopup.value);
+    localStorage.setItem('userPhone', userPhonePopup.value);
+    localStorage.setItem('userMessage', userMessagePopup.value);
+  }
+});
+
+form.addEventListener('submit', function () {
+  if (isStorageSupport) {
+    localStorage.setItem('userName', userName.value);
+    localStorage.setItem('userPhone', userPhone.value);
+    localStorage.setItem('userMessage', userMessage.value);
+  }
+});
